f4c8b4f019cde3d8da1a6d1cd0f5bdb9473f0afd
Dr. Safi <sfmissive@gmail.com>
2018-08-22 22:28:35 +0300

    Initial upload.

---
+++ setup/entrypoint.js
@@ -0,0 +1,25 @@
+/**
+ * @description Creates properties for instance corresponding to the json objects' key-value pairs.
+ * Basically an equivalent to Object.assign()
+ * @action creates class instance properties.
+ * @param  {object} jsonData key-value of object will end up as property-value of class.
+ **/
+function usingGenericInstance({object, jsonData}) {
+    if(jsonData) {
+        Object.entries(jsonData).forEach(([key, value]) => {
+            object[key] = value
+        })
+    }
+    object.jsonData = jsonData // to keep track of populated instances.
+}
+
+function usingThis({ jsonData, object = this }) {
+    // console.log(jsonData)
+    usingGenericInstance({object, jsonData})
+}
+
+export { 
+    usingGenericInstance as default,
+    usingGenericInstance,
+    usingThis
+}
